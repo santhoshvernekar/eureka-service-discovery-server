4a9b800ea5a632aae5684aa51aadbc78dded1cb3
Santhosh <santoshmv121@gmail.com>
2021-02-06 14:27:51 +0530

    Initial Commit

---
+++ src/test/java/com/santhosh/vernekar/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhosh.vernekar.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
